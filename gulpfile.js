7e63c106a9ba60792dcf5dfc7bed7032d48185c9
Harish V <harishv207@gmail.com>
2018-06-14 21:04:20 +0800

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+// including plugins
+var gulp = require('gulp'), 
+	uglify = require("gulp-uglify"),
+	minifyCss = require("gulp-minify-css"),
+	sass = require('gulp-sass');
+
+// minify js
+gulp.task('minify-js', function () {
+	gulp.src('./assets/js/*.js') // path to your files
+		.pipe(uglify())
+		.pipe(gulp.dest('assets/js/minified'));
+});
+
+// minify css
+gulp.task('minify-css', function () {
+	gulp.src('./assets/css/*.css') // path to your file
+		.pipe(minifyCss())
+		.pipe(gulp.dest('assets/css/minified'));
+});
+
+gulp.task('sass', function () {
+	return gulp.src('./assets/css/*.sass')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(gulp.dest('assets/css'));
+});
+
+gulp.task('watch-all', function () {
+	gulp.watch('./assets/js/*.js', ['minify-js']);
+	gulp.watch('./assets/css/*.css', ['minify-css']);
+	gulp.watch('./assets/css/*.sass', ['sass']);
+});
+
+// default gulp task - just run gulp in command line
+gulp.task('default', ['minify-js', 'sass', 'watch-all'], function() {
+});
