b844e802ea104735013c399f5f93ee94bfd0ec80
Harish V <harishv207@gmail.com>
2018-06-15 18:51:34 +0800

    Navbar color changes on scroll

--- assets/js/myjs.js
+++ assets/js/myjs.js
@@ -4,3 +4,10 @@ $(document).on('click', '[data-toggle="lightbox"]', function(event) {
 		wrapping: true
 	});
 });
+
+$(function () {
+  $(document).scroll(function () {
+    var $nav = $(".fixed-top");
+    $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+  });
+});
