e4d55eae51adc93b881cbed0aa18bb86cc892166
Harish V <harishv207@gmail.com>
2018-12-08 13:30:36 +0800

    Adding smooth scroll to divs

--- assets/js/myjs.js
+++ assets/js/myjs.js
@@ -1,48 +1,50 @@
-$(document).on('click', '[data-toggle="lightbox"]', function(event) {
-	event.preventDefault();
-	$(this).ekkoLightbox({
-		wrapping: true
+$(document).ready(function(){
+	$(document).on('click', '[data-toggle="lightbox"]', function(event) {
+		event.preventDefault();
+		$(this).ekkoLightbox({
+			wrapping: true
+		});
 	});
-});
 
-$(function () {
-	$(document).scroll(function () {
-		var $nav = $(".fixed-top");
-		$nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+	$(function () {
+		$(document).scroll(function () {
+			var $nav = $(".fixed-top");
+			$nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+		});
 	});
+
+	$("#show-more-btn").click(function() {
+		$("#about-more").show();
+	});
+
+	function loadBg() {
+		var backgroundImages = ["cover.jpg", "IMG_4478.jpg", "IMG_4481.jpg", "IMG_4511.jpg", "IMG_4527.jpg", "KEL_7799.jpg", "KEL_8222.jpg"];
+
+		var currIndex = 1;
+		setInterval(function() {
+			if(currIndex == backgroundImages.length) {
+				currIndex = 0;
+			}
+
+			var img = backgroundImages[currIndex];
+
+			$('.cover').css({"background-image":"url('./assets/images/bg-images/" + img + "')"});	
+			currIndex++;
+		}, 7000);
+	}
+	window.onload = loadBg();
+
+	// lazy load images
+	var myLazyLoad = new LazyLoad();
+
+	window.sr = ScrollReveal({
+		scale: 0.7,
+		duration: 700
+	});
+	sr.reveal('#about');
+	sr.reveal('#services');
+	sr.reveal('#portfolio');
+	sr.reveal('#clients');
+	sr.reveal('#connect');
+	sr.reveal('#contact');
 });
-
-$("#show-more-btn").click(function() {
-	$("#about-more").show();
-});
-
-function loadBg() {
-	var backgroundImages = ["cover.jpg", "IMG_4478.jpg", "IMG_4481.jpg", "IMG_4511.jpg", "IMG_4527.jpg", "KEL_7799.jpg", "KEL_8222.jpg"];
-
-	var currIndex = 1;
-	setInterval(function() {
-		if(currIndex == backgroundImages.length) {
-			currIndex = 0;
-		}
-
-		var img = backgroundImages[currIndex];
-
-		$('.cover').css({"background-image":"url('./assets/images/bg-images/" + img + "')"});	
-		currIndex++;
-	}, 7000);
-}
-window.onload = loadBg();
-
-// lazy load images
-var myLazyLoad = new LazyLoad();
-
-window.sr = ScrollReveal({
-	scale: 0.7,
-	duration: 700
-});
-sr.reveal('#about');
-sr.reveal('#services');
-sr.reveal('#portfolio');
-sr.reveal('#clients');
-sr.reveal('#connect');
-sr.reveal('#contact');
